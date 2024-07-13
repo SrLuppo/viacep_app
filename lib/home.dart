df6b281232208b212af310b3babdda6389d71677
adelano <adelano@softwar.com.br>
2024-07-12 21:59:49 -0300

    app-via-cep

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -8,8 +10,62 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController cepController = TextEditingController();
+  String rua = "", bairro = "" , uf = "";
+
+  void getCep() async {
+    String url = "https://viacep.com.br/ws/" + cepController.text + "/json";
+    http.Response response = await http.get(Uri.parse(url));
+    print(response.statusCode.toString());
+    print(response.body);
+
+    Map<String, dynamic> cepData = jsonDecode(response.body);
+
+    rua = cepData["logradouro"];
+    bairro = cepData["bairro"];
+    uf = cepData["uf"];
+
+    setState(() {
+      
+    });
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("Via Cep App"),),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextField(
+                controller: cepController
+              ),
+              ElevatedButton(
+                onPressed: getCep,
+                child: Text("Consultar")
+              ),
+              Text("Rua: $rua",
+                style: TextStyle(
+                  fontSize: 20
+                ),
+              ),
+              Text("Bairro: $bairro",
+                style: TextStyle(
+                  fontSize: 20
+                ),
+              ),
+              Text("UF: $uf",
+                style: TextStyle(
+                  fontSize: 20
+                ),
+              )
+              ],) ,
+        ),
+      ),
+    );
   }
 }
